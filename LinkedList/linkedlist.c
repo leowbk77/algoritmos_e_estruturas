56375ffd7ed27476976a23e6315c3e898f940387
Leonardo <leonardo.mf@ufu.br>
2023-09-25 21:34:54 -0300

    update llist

--- LinkedList/linkedlist.c
+++ LinkedList/linkedlist.c
@@ -22,6 +22,10 @@ linkedlist *create_list(){
     return list;
 }
 
+int list_free(linkedlist *list){
+    return SUCCESS;
+}
+
 int insert_front(data content, linkedlist *list){
     if(list == NULL){
         return INVALID_NULL_POINTER;
@@ -37,9 +41,56 @@ int insert_front(data content, linkedlist *list){
 }
 
 int insert_back(data content, linkedlist *list){
+    if(list == NULL){
+        return INVALID_NULL_POINTER;
+    } else {
+        llist_node *node = malloc(sizeof(llist_node));
+        if(node == NULL) return OUT_OF_MEMORY;
 
+        node->nodeData = content;
+        node->next = NULL;
+
+        if(list->head == NULL){
+            list->head = node;
+        } else {
+            llist_node *auxNode = list->head;
+            while(auxNode->next != NULL){
+                auxNode = auxNode->next;
+            }
+            auxNode->next = node;
+        }
+        list->size++;
+        return SUCCESS;
+    }
 }
 
 int insert_in(int position, data content, linkedlist *list){
+    if(list == NULL){
+        return INVALID_NULL_POINTER;
+    } else {
+        if(position > list->size) return OUT_OF_RANGE;
 
+        llist_node *node = malloc(sizeof(llist_node));
+        if(node == NULL) return OUT_OF_MEMORY;
+
+        node->nodeData = content;
+
+        if(position == 0){
+            node->next = list->head;
+            list->head = node;
+        } else {
+            llist_node *auxNodeOne = list->head;
+            llist_node *auxNodeTwo = list->head->next;
+            int i = 1;
+            while(i < position){
+                auxNodeOne = auxNodeOne->next;
+                auxNodeTwo = auxNodeTwo->next;
+                i++;
+            }
+            auxNodeOne->next = node;
+            node->next = auxNodeTwo;
+            list->size++;
+        }
+        return SUCCESS;
+    }
 }
--- LinkedList/linkedlist.h
+++ LinkedList/linkedlist.h
@@ -11,13 +11,15 @@ typedef struct data data;
 typedef struct linkedlist linkedlist;
 
 struct data{
+    int placeholderInt;
     int placeholderVector[10];
     char placeholderString[10];
 };
 
 linkedlist *create_list();
+int list_free(linkedlist *list);
 int insert_front(data content, linkedlist *list);
 int insert_back(data content, linkedlist *list);
-int insert_in(int position, data content, linkedlist *list);
+int insert_in(int position, data content, linkedlist *list); // first pos is 0
 
 #endif
